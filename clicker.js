2259500093a4bfacf32816f87a5ea65de5b0fc5c
Rhi <codingmaniac796@gmail.com>
2022-03-17 13:39:30 -0600

    hey look some functionality

--- clicker.js
+++ clicker.js
@@ -1,1 +1,30 @@
 //this is the javascript for the CLICKER side of the game
+//vars
+var open = false;
+//functs
+
+//onload
+$(document).ready(function () {
+  //onclick handlers
+  $("#clickerContain").click(function () {
+    if (!open) {
+      $(this).css("margin-top", "2px;");
+      open = true;
+    }
+  });
+
+  $("#clickClose").click(function () {
+    $("#clickerContain").css("margin-top", "-400px;");
+    open = false;
+  });
+
+  //updating elements
+  function update1F() {
+    if (open) {
+      $("#clickClose").show();
+    } else {
+      $("#clickClose").hide();
+    }
+  }
+  setInterval(update1F, 1000 / 60);
+});
